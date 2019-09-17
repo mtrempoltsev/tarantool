469af0b58b27d84c9d68e2886b04b5aae5844510
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2019-09-18 00:39:02 +0200

    app: fix boolean handling in argparse module
    There was a complaint that tarantoolctl --show-system option is
    very hard to use. It incorrectly parsed passed values, and
    provided strange errors.
    
        tarantoolctl cat --show-system true
        Bad input for parameter "show-system". Expected boolean, got "true"
    
        tarantoolctl cat --show-system 1
        Bad input for parameter "show-system". Expected boolean, got "1"
    
        tarantoolctl cat --show-system=true
        Bad input for parameter "show-system". Expected boolean, got "true"
    
    First of all, appeared that the complaining people didn't read
    documentation in 'tarantoolctl --help'. It explicitly says, that
    '--show-system' should go after a file name, and does not have a value.
    
    Secondly, even having taken the documentation into account, the
    errors indeed look ridiculous. 'Expected boolean, got "true"'
    looks especially weird.
    
    The problem appeared to be with argparse module, how it parses
    boolean parameters, and how stores parameter values not specified
    in a command line.
    
    All parameters were parsed into a dictionary: parameter name ->
    value. If a name is alone (no value), then it is boolean true.
    Otherwise it was always a string value. An attempt to specify
    an explicit parameter value 'true' led to storing string 'true' in
    that dictionary.
    
    Consequential check for boolean parameters was trivial:
    type(value) == 'boolean', which was obviously wrong, and didn't
    pass for 'true' string, but passed for an empty value.
    
    Closes #4076

--- test/app/argparse.test.lua
+++ test/app/argparse.test.lua
@@ -1,4 +1,6 @@
 -- internal argparse test
+test_run = require('test_run').new()
+test_run:cmd("push filter ".."'\\.lua.*:[0-9]+: ' to '.lua:<line>\"]: '")
 
 argparse = require('internal.argparse').parse
 
@@ -28,3 +30,21 @@ argparse({'--verh=42'}, {{'verh', 'boolean+'}})
 argparse({'--verh=42'}, {'niz'})
 argparse({'--super-option'})
 argparse({'tarantoolctl', 'start', 'instance', '--start=lalochka', 'option', '-', 'another option'})
+
+--
+-- gh-4076: argparse incorrectly processed boolean parameters,
+-- that led to problems with tarantoolctl usage.
+--
+params = {}
+params[1] = {'flag1', 'boolean'}
+params[2] = {'flag2', 'boolean'}
+params[3] = {'flag3', 'boolean'}
+params[4] = {'flag4', 'boolean'}
+params[5] = {'flag5', 'boolean'}
+args = {'--flag1', 'true', '--flag2', '1', '--flag3', 'false', '--flag4', '0', '--flag5', 'TrUe'}
+argparse(args, params)
+
+args = {'--flag1', 'abc'}
+argparse(args, params)
+
+test_run:cmd("clear filter")
